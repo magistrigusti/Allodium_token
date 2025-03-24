57adfa4c7e25174eb790201a09058ef32cf2fd40
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-24 21:24:21 +0200

    0.7b1

--- tests/AllodiumJettonWallet.spec.ts
+++ tests/AllodiumJettonWallet.spec.ts
@@ -48,6 +48,63 @@ describe('AllodiumJettonWallet', () => {
         const data = await userWallet.getWalletData();
 
         expect(data.balance.toNumber()).toBe(0);
+        expect(data.owner.equals(deployer.address)).toBeTruthy();
+        expect(data.master.equals(masterWallet.address)).toBeTruthy();
+    });
+
+    it('should correctly process internal token transfer', async () => {
+        const transferAmount = toNano('10');
+
+        await userWallet.sendInternalMessage({
+            from: masterWallet.address,
+            value: toNano('0.05'),
+            body: {
+                $$type: 'TokenTransferInternal',
+                query_id: 1n,
+                amount: transferAmount,
+                from: masterWallet.address,
+                response_destination: masterWallet.address,
+                forward_ton_amount: 0n,
+                forward_payload: Buffer.alloc(0)
+            }
+        });
+
+        const dataAfterTransfer = await userWallet.getWalletData();
+        expect(dataAfterTransfer.balance.toString()).toBe(transferAmount.toString());
+    });
+
+    it('should burn tokens correctly', async () => {
+        const initialAmount = toNano('20');
+        const burnAmount = toNano('5');
+
+        await userWallet.sendInternalMessage({
+            from: masterWallet.address,
+            value: toNano('0.05'),
+            body: {
+                $$type: "TokenTransferinternal",
+                query_id: 2n,
+                amount: initialAmount,
+                from: masterWallet.address,
+                response_estination: masterWallet.address,
+                forward_ton_amount: 0n,
+                forward_payoad: Buffer.alloc(0)
+            }
+        });
+
+        await userWallet.send(
+            deployer.getSender(),
+            { value: toNano('0.05')},
+            {
+                $$type: 'Burn',
+                query_id: 3n,
+                amount: burnAmount,
+                response_destination: deployer.address,
+                custom_payload: null,
+            }
+        );
+
+        const dataAfterBurn = await userWallet.getWalletData();
+        expect(dataAfterBurn.balance.toString()).toBe(toNano('15').toString());
     });
 
 });
