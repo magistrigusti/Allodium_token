4a752eac36ede7b0c4f87e74ce1954a2699ef625
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-26 10:25:34 +0200

    0.8.2

---
+++ scripts/deploy-minter.ts
@@ -0,0 +1,33 @@
+import { toNano, Cell } from '@ton/core';
+import { NetworkProvider } from '@ton/blueprint';
+// import { AllodiumJettonMinter } from '../wrappers/AllodiumJettonMinter';
+
+export async function run(provider: NetworkProvider) {
+    const sender = provider.sender(); // тот, кто деплоит
+    const owner = sender.address;
+
+
+    const content = new Cell(); // сюда можно вставить jetton metadata (URI, JSON и т.д.)
+    const rewardAddress = owner;
+    const inflationAddress = owner;
+    const nftRegistry = owner;
+
+
+    const minter = provider.open(
+        AllodiumJettonMinter.createFromConfig(
+            {
+                owner,
+                content,
+                rewardAddress,
+                inflationAddress,
+                nftRegistry
+            },
+            await provider.compile('AllodiumJettonMinter')
+        )
+    );
+
+
+    await minter.sendDeploy(sender, toNano('0.05'));
+
+    console.log('✅ AllodiumJettonMinter deployed at:', minter.address.toString());
+}
--- wrappers/AllodiumJettonWallet.compile.ts
+++ wrappers/AllodiumJettonWallet.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/AllodiumJettonWallet.tact',
+    target: 'contracts/AllodiumJettonMiner.tact',
     options: {
         debug: true,
     },
