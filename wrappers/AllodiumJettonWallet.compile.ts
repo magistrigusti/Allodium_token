59616834a1577e95c86847b220ae5170da68fc57
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-28 11:21:06 +0300

    0.8.7a

--- wrappers/AllodiumJettonWallet.compile.ts
+++ wrappers/AllodiumJettonWallet.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/AllodiumJettonWallet.tact',
+    target: 'contracts/AllodiumJettonMaster.tact',
     options: {
         debug: true,
     },
